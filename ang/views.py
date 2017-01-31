7a68fc7c5415f3abea8f57b6fb3d7e7af16a4b21
Chris Ward <chriswardchrisward@gmail.com>
2017-01-31 10:28:14 -0800

    angular templates in Django I

--- ang/views.py
+++ ang/views.py
@@ -25,4 +25,4 @@ from django.shortcuts import render
 
 
 def get_angular_template(request, path=None):
-    return render(request, "ang/blog-list.html", {})
+    return render(request, "ang/app/blog-list.html", {})
